778ab30388771fa7d24c1c01e94c5397a5e12641
Murad <qurbanbeyli.murad@gmail.com>
2019-07-28 19:10:38 +0400

    refactoring

--- gulpFile.js
+++ gulpFile.js
@@ -5,6 +5,9 @@ const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
+// const minify = require('gulp-minify');
+const minifyCSS = require('gulp-minify-css');
+const browserSync = require('browser-sync').create();
 
 function cssProcessing(callback){
     
@@ -12,16 +15,39 @@ function cssProcessing(callback){
     .pipe(sourcemaps.init())
     .pipe(sass({
         errorLogToConsole:true,
-        outputStyle: 'compressed'
+        // outputStyle: 'compressed'
     }))
     .on('error',console.error.bind(console))
     .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(gulp.dest('./prodaction/css/'))
+    .pipe(minifyCSS())
     .pipe(rename({suffix: '.min'}))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./prodaction/css/'))
+    .pipe(browserSync.stream());
+     
     callback();
 }
 
 gulp.task(cssProcessing);
 
-gulp.task('default', gulp.series(cssProcessing,whach))
+function liveServer(callback) {
+    browserSync.init({
+        server: {
+            baseDir:"./prodaction"
+        },
+        port: 3000
+    });
+    callback();
+}
+
+
+
+function watchProcessing(){
+    gulp.watch("./development/sass/**/*",cssProcessing);
+}
+
+
+gulp.task('default', gulp.series(cssProcessing,watchProcessing));
+
+gulp.task(liveServer);
